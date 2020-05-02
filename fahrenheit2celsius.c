a2541deb2e22e051f4f05c018bb5fd22f264aa73
David Shin <davidshin444@gmail.com>
2020-05-02 06:53:26 -0700

    Make classic Fahrenheit to Celsius converter

---
+++ fahrenheit2celsius.c
@@ -0,0 +1,23 @@
+/* print Fahrenheit to Celsius table
+   for f = 0, 20, ..., 300 */
+
+# import <stdio.h>
+
+int main()
+{
+    int lower, upper, step;
+    float fahr, celsius;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%4.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
